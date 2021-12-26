47c292600e518fc42096a03f9158280e2c7eed93
KuldeepSingh <kul8003995609deep@gmail.com>
2021-12-26 23:09:36 +0530

    added variable and datatype

---
+++ variable-and-datatype/script.js
@@ -0,0 +1,40 @@
+//Data types in javascript
+//Number :- normal decimal
+let myNum=53;
+console.log('myNum is a '+typeof(myNum));
+
+//BigInt :-huge number big decimal end with n
+let myBigInt=3987592384759238475029348572093457n;
+console.log('myBigInt is a '+typeof(myBigInt));
+
+//String :- collection of characters
+let myString=`Hello world!
+How are you;`;
+console.log('myString is a '+typeof(myString));
+
+//boolean :- hold true or false
+let myBool = true;
+console.log('myBool is a '+typeof(myBool));
+
+//undefined :- variable that is not initialized
+let myUndef;
+console.log('myUndef is a '+typeof(myUndef));
+
+//null : variable that is initializied but value is nothing
+let myNull=null;
+console.log('myNull is a '+typeof(myNull));
+
+//objects :
+let myValue ={ name:"Hellow",value:123};
+console.log('myValue is a '+typeof(myValue));
+
+//symboles
+let mySymbole = Symbol('foo');
+console.log('mySymbole is a '+typeof(mySymbole));
+
+
+
+
+
+
+
